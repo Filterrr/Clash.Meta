12c45dfc3ddc66e694b1020bf1fd3f62725703df
Filterrr <36611101+Filterrr@users.noreply.github.com>
2026-05-25 19:10:17 +0800

    Update config.go

--- config/config.go
+++ config/config.go
@@ -967,23 +967,6 @@ func parseProxies(cfg *RawConfig) (proxies map[string]C.Proxy, providersMap map[
 	}
 
 	SetProxyNameList(proxyList)
-	SetProxyNameList(proxyList)
-
-	SetProxyNameList(proxyList)
-	SetProxyNameList(proxyList)
-	SetProxyNameList(proxyList)
-	SetProxyNameList(proxyList)
-	SetProxyNameList(proxyList)
-	SetProxyNameList(proxyList)
-	SetProxyNameList(proxyList)
-	SetProxyNameList(proxyList)
-	SetProxyNameList(proxyList)
-	SetProxyNameList(proxyList)
-	SetProxyNameList(proxyList)
-	SetProxyNameList(proxyList)
-	SetProxyNameList(proxyList)
-	SetProxyNameList(proxyList)
-	SetProxyNameList(proxyList)
 	return proxies, providersMap, nil
 }
 
